b5456d42a222956ad4bd5d9794df02e6f688d31c
nguyenhuy.chung <nguyenhuy.chung@efce806b-3334-0410-bc95-0fbd495663c4>
2007-07-14 07:36:23 +0000

    Add errors header file

---
+++ trunk/isolib/errors.h
@@ -0,0 +1,10 @@
+#ifndef ERRORS_H_
+#define ERRORS_H_
+#define	ERR_OVRLEN	100
+/*Define the const error of iso message*/
+const errmsg errdef[] ={
+		{1, ""}, /*The length of field is to long*/
+		{2, ""}, /*The length of field is not correct*/
+		{3, ""}
+}; /*The format error*/
+#endif /*ERRORS_H_*/
