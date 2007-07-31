011c1e9d15062a288442d9e4411043b252f37c1a
nguyenhuy.chung <nguyenhuy.chung@efce806b-3334-0410-bc95-0fbd495663c4>
2007-07-31 04:42:20 +0000

    rename err_hld --> handle_err

--- isolib/errors.h
+++ isolib/errors.h
@@ -77,12 +77,12 @@ int check_fld(char *value, int idx, const isodef *def);
  */
 void sys_err(int err_code, char *filename);
 
-/*! \fn void err_hld(int err_code, int err_type, char *desc)
+/*! \fn void handle_err(int err_code, int err_type, char *desc)
  * 		\brief This function is used to write the error of one field to the log file
  * 		\param err_code the error code that recieved when error appear
  * 		\param err_type is type of error, if err_type = 1 is the system error else is the iso error
  * 		\param desc is the description of this error
  * 		\output: the desc of this error is written to log file (file name)
  */
-void err_hld(int err_code, int err_type, char *desc);
+void handle_err(int err_code, int err_type, char *desc);
 #endif /*ERRORS_H_*/
