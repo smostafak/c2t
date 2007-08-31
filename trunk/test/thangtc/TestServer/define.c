d64834f23f8ef45f2eb55ccf6cb5e37f11c51648
thangh3t <thangh3t@efce806b-3334-0410-bc95-0fbd495663c4>
2007-08-31 07:19:15 +0000

    Update test program

---
+++ trunk/test/thangtc/TestServer/define.c
@@ -0,0 +1,12 @@
+#include "define.h"
+#include <stdio.h>
+int test1()
+{
+	printf("Day la ham thu nhat");
+	return 1;
+}
+int test2()
+{
+	printf("Day la ham thu hai\Enter to continue...");
+	return 1;
+}
